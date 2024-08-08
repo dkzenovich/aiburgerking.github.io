d6f0991e8cfeddaf3b5bdf37be2d102f2d132db2
dkzenovich <110024362+dkzenovich@users.noreply.github.com>
2024-08-08 17:18:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -55,10 +55,34 @@ function sendOrder() {
         cart: cart,
         total: total
     };
-    debugLog('Sending data to bot: ' + JSON.stringify(data));
-    tg.sendData(JSON.stringify(data));
+    debugLog('Attempting to send data to bot: ' + JSON.stringify(data));
+    try {
+        tg.sendData(JSON.stringify(data));
+        debugLog('Data sent successfully');
+    } catch (error) {
+        debugLog('Error sending data: ' + error.message);
+    }
+    debugLog('tg.initDataUnsafe: ' + JSON.stringify(tg.initDataUnsafe));
+    debugLog('tg.initData: ' + tg.initData);
 }
 
+// Инициализация
 debugLog('Web App initialized');
 debugLog('Telegram WebApp version: ' + tg.version);
 debugLog('Platform: ' + tg.platform);
+
+// Добавим обработчик для закрытия веб-приложения
+tg.onEvent('viewportChanged', function(){
+    debugLog('Viewport changed');
+});
+
+tg.onEvent('backButtonClicked', function(){
+    debugLog('Back button clicked');
+    tg.close();
+});
+
+// Отобразим MainButton, если корзина не пуста
+if (total > 0) {
+    tg.MainButton.setText("Оплатить");
+    tg.MainButton.show();
+}
